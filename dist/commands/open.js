d3c7d142aa8fa587445aa68a1fcc96370dc53938
Tjird <tjeerd@tjird.nl>
2019-10-28 21:56:40 +0100

    rewgatf

--- dist/commands/open.js
+++ dist/commands/open.js
@@ -102,7 +102,7 @@ function () {
             card = _context2.sent;
             animation = "nonrare";
             if (player_info.rareflag === 1) animation = "rare";
-            if (player_info.rareflag !== 3 && player_info.rating >= 83 || player_info.rareflag === 3 && player_info.rating <= 82 || player_info.rareflag === 47 || player_info.rareflag === 48) animation = "board";
+            if (player_info.rareflag !== 3 && player_info.rating >= 83 || player_info.rareflag === 3 && player_info.rating <= 82 || player_info.rareflag === 48) animation = "board";
             if (player_info.rareflag !== 3 && player_info.rating > 85 || player_info.rareflag === 3 && player_info.rating >= 83 || player_info.rareflag === 12) animation = "walkout";
             _context2.next = 29;
             return (0, _general.getPackById)(args[0]);
